cf0f3a8125f23e488671ef751e0f1d640b076479
Flavio F. Contreras-Torres <contreras.flavio@tec.mx>
2026-05-17 14:44:37 -0600

    docs: add SPDX license identifier to all source files

--- harmonsmile/__main__.py
+++ harmonsmile/__main__.py
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
 """
 Entry point for running harmonsmile as a module.
 
--- harmonsmile/pubchem.py
+++ harmonsmile/pubchem.py
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
 """
 PubChem REST API client for harmonsmile.
 """
